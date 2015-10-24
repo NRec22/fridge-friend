dd51f3bb07c8c7199dea6db16629b563f7a532bb
NRec22 <nevenrecchia@gmail.com>
2015-10-24 16:48:10 -0400

    example server commands

--- fridgesrc/example.js
+++ fridgesrc/example.js
@@ -8,13 +8,18 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
 
+ //example commands
   insertDocuments(db, function() {
     updateDocument(db, function() {
-      db.close();
+      deleteDocument(db, function() {
+        // findDocuments(db, function() {
+        //   db.close();
+        // });
+        db.close();
+      });
     });
   });
 });
-
 
 var insertDocuments = function(db, callback) {
   // Get the documents collection
@@ -43,3 +48,28 @@ var updateDocument = function(db, callback) {
     callback(result);
   });
 }
+
+var deleteDocument = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.deleteOne({ a : 3 }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Removed the document with the field a equal to 3");
+    callback(result);
+  });
+}
+
+// var findDocuments = function(db, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Find some documents
+//   collection.find({}).toArray(function(err, docs) {
+//     assert.equal(err, null);
+//     assert.equal(2, docs.length);
+//     console.log("Found the following records");
+//     console.dir(docs);
+//     callback(docs);
+//   });
+// }
