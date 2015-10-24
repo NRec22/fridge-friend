852a3a78e46eac91afca903b0fd58eac7bc212b7
NRec22 <nevenrecchia@gmail.com>
2015-10-24 17:40:17 -0400

    insert

--- fridgesrc/first_attempt.js
+++ fridgesrc/first_attempt.js
@@ -8,24 +8,21 @@ MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
 
-  insertDocuments(db, function() {
-    updateDocument(db, function() {
-      db.close();
-    });
-  });
+  // insertDocuments(db, function() {
+  //   updateDocument(db, function() {
+  //     db.close();
+  //   });
+  // });
 });
 
+var insertFood = function(db, food, number, expiration){
+    var collection = db.collection('inventory');
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertOne(
-     {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
-    callback(result);
-  );
+    // insert the food and its attributes
+    collection.insert(
+        {
+            "quantity": number,
+            "type": food,
+            "exp": expiration
+        });
 }
